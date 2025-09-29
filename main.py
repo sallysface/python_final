df30ffe4a6d9a7f60c7b133f4dae2e774c54d286
sallysface <eva.gilbert@ucas-edu.net>
2025-09-29 14:39:32 -0600

    meow
    Co-authored-by: DaddyMqnkey72 <DaddyMqnkey72@users.noreply.github.com>
    Co-authored-by: Hanbanan03 <Hanbanan03@users.noreply.github.com>
    Co-authored-by: akanomdeplume <akanomdeplume@users.noreply.github.com>
    Co-authored-by: zaddyphwithafatty <zaddyphwithafatty@users.noreply.github.com>

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 
 import random
 #Isaiah - Making Board
-def hangman(wrong)
-    if wrong< 1:
+def hangman(wrong):
+    if wrong < 1:
         return """|--|
                   |
                   |
                   |
                   |____"""
-    elif wrong< 2:
+    elif wrong == 2:
         return """|--|
                   |  O
                   |
@@ -29,7 +29,7 @@ def hangman(wrong)
                    |  /|\\
                    |
                    |_____"""
-        elif wrong == 5:
+    elif wrong == 5:
             return """
                     |---|
                     |   O
@@ -37,7 +37,7 @@ def hangman(wrong)
                     |  /
                     |_____"""
 
-        elif wrong == 6:
+    elif wrong == 6:
             return """
                     |---|
                     |   O
@@ -54,17 +54,62 @@ def current(guessed, word):
             blanks += letter
         else:
             blanks += "_"
-    return
-    print(f'Word: \n{blanks} Guesses: \n{guessed}')
+    return f'Word:\n{blanks} Guesses: \n{guessed}'
 
 def guess():
-    word = input("Guesse a letter to start\n").strip().lower()
+    word = input("Guess a letter\n").strip().lower()
     return word
 
+def main():
+     bank = ["poo", "meow", "monkey", "fluroide", 
+             "pyonyang","outkast", "isaiah","party","nice", "frog", 
+             "princess","sixseven", "femboy", "ducks", "beehive", 
+             "ambiguous", "musical", "mango", "mustard" , "fortyone" , 
+             "unc", "jba" ,"aot" , "beethoven", "hannah", "abyss", 
+             "haiku", "pixel", "pickel", "pajama", "galaxy", "wellspring", 
+             "witchcraft", "puppy", "grogginess", "pneumonia", "oxygen", 
+             "gossip", "onyx", "calix", "voodoo", "gnarly", "unfixable", 
+             "wizard", "microwave", "jaywalk", "rhythem", "iceberg", "titanic", 
+             "jazz", "guitar", "buckaroo", "buddy", "embezzeled","fishook", 
+             "beekeeper", "ballistic", "maneuver", "resuscitate","judgment", 
+             "shorty", "baddie", "doctor"]
 #Hannah - Wrong guesses
+ 
+wrong_guesses = 0
+guessed_letters = []
+current_word = random.choice(bank)
+play = "play"
+while play == "play":
+
+
+    print(f"Hangman{wrong_guesses}")
+
+
+    print(current(guessed_letters, current_word))
+
+
+    letter = guess()
+    guessed_letters.append(letter)
+
+
+    if letter not in current_word:
+      wrong_guesses += 1
+
 
 #Gyan - Blanks
-
+blanks = ""
+for letter in current_word:
+    if letter in guessed_letters:
+        blanks += letter
+else:
+    blanks =+"_"
+    if blanks == current_word:
+        print("You win!")
+        play = "win"
+        if wrong_guesses >= 6:
+            hangman(wrong_guesses)
+            print("you lose!")
+            play = "lose"
 #Enzo - Print/play again rand letter
 
 again = input("do you want to play again?(yes/no)?/n").strip().lower()
