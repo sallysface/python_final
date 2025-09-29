b65f2752d05f790f7f2a2205823a16e237e64091
sallysface <eva.gilbert@ucas-edu.net>
2025-09-29 19:42:24 +0000

    codid

--- main.py
+++ main.py
@@ -6,22 +6,60 @@ def hangman(wrong)
     if wrong< 1:
         return """|--|
                   |
-                  |_
-                  |__"""
+                  |
+                  |
+                  |____"""
     elif wrong< 2:
         return """|--|
                   |  O
                   |
-                  |___"""   
-    elif wrong < 3:
-        return """|--|
-                  |  O
-                  |  |
-                  |___"""
-    
-             
+                  |
+                  |_____"""   
+    elif wrong == 3:
+        return """
+                  |---|
+                  |   O 
+                  |  /|
+                  | 
+                  |_____"""
+    elif wrong == 4:
+        return """
+                   |---|
+                   |   O
+                   |  /|\\
+                   |
+                   |_____"""
+        elif wrong == 5:
+            return """
+                    |---|
+                    |   O
+                    |  /|\\
+                    |  /
+                    |_____"""
+
+        elif wrong == 6:
+            return """
+                    |---|
+                    |   O
+                    |  /|\\
+                    |  / \\
+                    |_____"""
+                    
 
 #Eva - Guessing letters
+def current(guessed, word):
+    blanks = ""
+    for letter in word:
+        if letter in guessed:
+            blanks += letter
+        else:
+            blanks += "_"
+    return
+    print(f'Word: \n{blanks} Guesses: \n{guessed}')
+
+def guess():
+    word = input("Guesse a letter to start\n").strip().lower()
+    return word
 
 #Hannah - Wrong guesses
 
