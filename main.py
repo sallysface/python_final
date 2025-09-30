533f4638b1038e3b783889f0cfb0656415e3a2a9
sallysface <eva.gilbert@ucas-edu.net>
2025-09-30 14:41:59 -0600

    Co-authored-by: DaddyMqnkey72 <DaddyMqnkey72@users.noreply.github.com> Co-authored-by: Hanbanan03 <Hanbanan03@users.noreply.github.com> Co-authored-by: zaddyphwithafatty <zaddyphwithafatty@users.noreply.github.com>

--- main.py
+++ main.py
@@ -3,48 +3,53 @@
 import random
 #Isaiah - Making Board
 def hangman(wrong):
-    if wrong < 1:
-        return """|--|
-                  |
-                  |
-                  |
-                  |____"""
-    elif wrong == 2:
-        return """|--|
-                  |  O
-                  |
-                  |
-                  |_____"""   
-    elif wrong == 3:
-        return """
-                  |---|
-                  |   O 
-                  |  /|
-                  | 
-                  |_____"""
+    if wrong == 6:
+     print("""___
+            |  O
+            | /|\\
+            |  /\\
+            |______
+    """)    
+    elif wrong  == 5:
+     print("""___
+            |  O
+            | /|\\
+            |  /
+            |______
+           """)   
+    
     elif wrong == 4:
-        return """
-                   |---|
-                   |   O
-                   |  /|\\
-                   |
-                   |_____"""
-    elif wrong == 5:
-            return """
-                    |---|
-                    |   O
-                    |  /|\\
-                    |  /
-                    |_____"""
+        print ("""___
+                 |  O
+                 | /|\\
+                 | 
+                 |______""")
+    elif wrong == 3:
+        print("""___
+                |  O
+                | /|
+                | 
+                |______""")
+    elif wrong == 2:
+        print("""___
+                |  O
+                | /
+                | 
+                |______""")
+    elif wrong == 1:
+        print("""___
+                |  O
+                | 
+                | 
+                |______""")
+    else:
+        print("""___
+                |  
+                | 
+                | 
+                |______""")
 
-    elif wrong == 6:
-            return """
-                    |---|
-                    |   O
-                    |  /|\\
-                    |  / \\
-                    |_____"""
-                    
+
 
 #Eva - Guessing letters
 def current(guessed, word):
@@ -61,50 +66,47 @@ def guess():
     return word
 
 def main():
-     bank = ["poo", "meow", "monkey", "fluroide", 
-             "pyonyang","outkast", "isaiah","party","nice", "frog", 
-             "princess","sixseven", "femboy", "ducks", "beehive", 
-             "ambiguous", "musical", "mango", "mustard" , "fortyone" , 
-             "unc", "jba" ,"aot" , "beethoven", "hannah", "abyss", 
-             "haiku", "pixel", "pickel", "pajama", "galaxy", "wellspring", 
-             "witchcraft", "puppy", "grogginess", "pneumonia", "oxygen", 
-             "gossip", "onyx", "calix", "voodoo", "gnarly", "unfixable", 
-             "wizard", "microwave", "jaywalk", "rhythem", "iceberg", "titanic", 
-             "jazz", "guitar", "buckaroo", "buddy", "embezzeled","fishook", 
-             "beekeeper", "ballistic", "maneuver", "resuscitate","judgment", "shorty", "baddie", "doctor"]
-#Hannah - Wrong guesses
- 
-wrong_guesses = 0
-guessed_letters = []
-current_word = random.choice(bank)
-play = "play"
-while play == "play":
+    bank = [
+        "poo", "meow", "monkey", "fluroide", 
+        "pyonyang","outkast", "isaiah","party","nice", "frog", 
+        "princess","sixseven", "femboy", "ducks", "beehive", 
+        "ambiguous", "musical", "mango", "mustard" , "fortyone" , 
+        "unc", "jba" ,"aot" , "beethoven", "hannah", "abyss", 
+        "haiku", "pixel", "pickel", "pajama", "galaxy", "wellspring", 
+        "witchcraft", "puppy", "grogginess", "pneumonia", "oxygen", 
+        "gossip", "onyx", "calix", "voodoo", "gnarly", "unfixable", 
+        "wizard", "microwave", "jaywalk", "rhythem", "iceberg", "titanic", 
+        "jazz", "guitar", "buckaroo", "buddy", "embezzeled","fishook", 
+        "beekeeper", "ballistic", "maneuver", "resuscitate","judgment", "shorty", "baddie", "doctor"
+    ]
+    #Hannah - Wrong guesses
 
+    wrong_guesses = 0
+    guessed_letters = []
+    current_word = random.choice(bank)
+    play = "play"
+    while play == "play":
 
-    print(f"Hangman{wrong_guesses}")
+        print(f"Hangman{wrong_guesses}")
 
+        print(current(guessed_letters, current_word))
 
-    print(current(guessed_letters, current_word))
+        letter = guess()
+        guessed_letters.append(letter)
 
+        if letter not in current_word:
+            wrong_guesses += 1
 
-    letter = guess()
-    guessed_letters.append(letter)
-
-
-    if letter not in current_word:
-      wrong_guesses += 1
-
-
-#Gyan - Blanks
-blanks = ""
-for letter in current_word:
-    if letter in guessed_letters:
-        blanks += letter
-else:
-    blanks =+"_"
-    if blanks == current_word:
-        print("You win!")
-        play = "win"
+        #Gyan - Blanks
+        blanks = ""
+        for letter in current_word:
+            if letter in guessed_letters:
+                blanks += letter
+            else:
+                blanks += "_"
+        if blanks == current_word:
+            print("You win!")
+            play = "win"
         if wrong_guesses >= 6:
             hangman(wrong_guesses)
             print("you lose!")
